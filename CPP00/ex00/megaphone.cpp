43897606f0384646eab72fee7f25376d92198656
Istvan Willmott <istvan.willmott@gmail.com>
2023-01-23 14:29:19 +1030

    ex00 added

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc > 1)
+	{
+		if (argc >= 3)
+		{
+			for (int x = 1; x < int (sizeof(argv)/2); x++)
+				for (int i = 0; i < int (strlen(argv[x])); i++)
+					putchar(toupper(argv[x][i]));
+			putchar('\n');
+		}
+		else
+		{
+			for (int i = 0; i < int (strlen(argv[1])); i++)
+				putchar(toupper(argv[1][i]));
+			putchar('\n');
+		}
+	}
+	else
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";	
+	return 0;
+}
