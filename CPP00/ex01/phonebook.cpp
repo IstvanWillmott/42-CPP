30e301f9822af76db2e28367ab2dd0dd89357f8e
Istvan Willmott <istvan.willmott@gmail.com>
2023-02-24 15:33:34 +1030

    Work on phonebook

--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using std::string;
+using std::cout;
+using std::cin;
 
 class Contact
 {
@@ -9,14 +11,22 @@ class Contact
 		string Nickname;
 		string PhoneNumber;
 		string DarkestSecret;	
-		Contact(string firstName, string lastName, string nickname, string phoneNumber, string darkestSecret)
-		{
-			FirstName = firstName;
-			LastName = lastName;
-			Nickname = nickname;
-			PhoneNumber = phoneNumber;
-			DarkestSecret = darkestSecret;	
-		}
+		Contact()
+        {
+            FirstName = "";
+            LastName = "";
+            Nickname = "";
+            PhoneNumber = "";
+            DarkestSecret = "";
+        }
+        Contact(string firstName, string lastName, string nickname, string phoneNumber, string darkestSecret)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Nickname = nickname;
+            PhoneNumber = phoneNumber;
+            DarkestSecret = darkestSecret;
+        }
 };
 
 class PhoneBook 
@@ -28,17 +38,84 @@ class PhoneBook
 			contacts = new Contact[8];
 			for (int i = 0; i < 8; i++)
 			{
-				contacts[i].FirstName = "";
-				contacts[i].LastName = "";
-				contacts[i].Nickname = "";
-				contacts[i].PhoneNumber = "";
-				contacts[i].DarkestSecret = "";
+				contacts[i] = Contact();
+			}
+		}
+};
+
+class ADD
+{
+	public:
+		string fname;
+		string lname;
+		string nname;
+		string pnumber;
+		string dsecret;
+		ADD()
+		{
+			cout << "First name: ";
+			cin >> fname;
+			cout << "\nLast name: ";
+			cin >> lname;
+			cout << "\nNick name: ";
+			cin >> nname;
+			cout << "\nPhone number: ";
+			cin >> pnumber;
+			cout << "\nDarkest secret: ";
+			cin >> dsecret;
+			cout << "\n";
+		}
+};
+
+class SEARCH
+{
+	public:
+		string fnameedit;
+		string lnameedit;
+		string nnameedit;
+		string pnumberedit;
+		SEARCH(){};
+		SEARCH(PhoneBook phonebook)
+		{
+			for (int i = 0; i < 7; i++)
+			{
+				fnameedit = phonebook.contacts[0].FirstName
+				cout << "|" + phonebook.contacts[0].FirstName + "|" + phonebook.contacts[0].LastName + "|" + phonebook.contacts[0].Nickname + "|" + phonebook.contacts[0].PhoneNumber + "|";
 			}
 		}
 };
 
 int main()
 {
+	string userinput;
+	int	curinput = 0;
+
 	PhoneBook phonebook;
-	phonebook.contacts
+	//phonebook.contacts;
+	while (true)
+	{
+		cin >> userinput;
+		if (userinput == "ADD")
+		{
+			ADD add;
+			phonebook.contacts[curinput].FirstName = add.fname;
+			phonebook.contacts[curinput].LastName = add.lname;
+			phonebook.contacts[curinput].Nickname = add.nname;
+			phonebook.contacts[curinput].PhoneNumber = add.pnumber;
+			phonebook.contacts[curinput].DarkestSecret = add.dsecret;
+			if (curinput == 7)
+				curinput = 0;
+			else
+				curinput++;	
+		}
+		else if (userinput == "SEARCH")
+		{
+			SEARCH search(phonebook);
+			
+		}
+		else if (userinput == "EXIT")
+		{
+			return 0;
+		}
+	}
 }
